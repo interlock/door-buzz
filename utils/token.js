621ae8637b4bad9e6277c68d104c3a22956c77ed
James Sapara <james.sapara@gmail.com>
2016-09-09 14:14:36 -0700

    Cleaned up some logs

--- utils/token.js
+++ utils/token.js
@@ -2,12 +2,11 @@
 var prefix = "G_";
 
 var input = process.argv[2];
-// console.log(input);
+
 var j = require(input);
 
-// console.log(j);
 for(var k in j) {
   let new_k = prefix + k.toUpperCase();
-  let v = j[k].replace(/"/g,'\"').replace(/\n/g,'\\n')
+  let v = j[k].replace(/"/g,'\"').replace(/\n/g,'\\n');
   process.stdout.write(new_k + "=\"" + v +"\"\n");
 }
